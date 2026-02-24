9bebd256fb687df687e9b2782f64230fce618e46
ALEX <ALEXTEEPEN16@ICLOUD.COM>
2026-02-24 20:34:25 +0400

    Use native script tags in layout for trackers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata, Viewport } from "next";
-import Script from "next/script";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -18,7 +17,6 @@ export const viewport: Viewport = {
 export const metadata: Metadata = {
   metadataBase: new URL(SITE_URL),
   verification: {
-    // CRITICAL: This is the ID from your archived WordPress site
     google: "GYWD36nzRWOa51TIlhIx2hj-UcaOwf_U-YYAr7DAzXU",
   },
   title: {
@@ -41,51 +39,44 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Native Injection: Forces trackers into raw View Source HTML */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RD6W1RGFS8');
+        `}} />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RD6W1RGFS8" />
+
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TKPJH55B');
+        `}} />
+
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MM5D885J');
+        `}} />
+      </head>
       <body className="antialiased">
-        {/* Google Analytics (GA4) */}
-        <Script id="ga-setup" strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-RD6W1RGFS8" />
-        <Script id="ga-config" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-RD6W1RGFS8');
-          `}
-        </Script>
-
-        {/* Google Tag Manager (GTM) */}
-        <Script id="gtm-setup-1" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TKPJH55B');
-          `}
-        </Script>
-        <Script id="gtm-setup-2" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MM5D885J');
-          `}
-        </Script>
-
-        {/* UserWay Accessibility */}
-        <Script
-          src="https://cdn.userway.org/widget.js"
-          data-account="wtfVmFNygS"
-          strategy="lazyOnload"
-        />
-
         <noscript>
           <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKPJH55B" height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
         </noscript>
         <noscript>
           <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MM5D885J" height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
         </noscript>
+
+        <script async src="https://cdn.userway.org/widget.js" data-account="wtfVmFNygS" />
 
         <JsonLd />
         <Header />
